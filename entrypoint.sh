22ad3781d8b2e8e1405882b50f086977799edb20
appleboy <appleboy.tw@gmail.com>
2023-04-24 22:28:58 +0800

    docs: configure Git to trust workspace ownership
    - Add a comment to configure git to trust the workspace despite the different owner.
    
    Signed-off-by: appleboy <appleboy.tw@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ set -eu
 
 export GITHUB="true"
 
+# Configure git to trust the workspace despite the different owner
 git config --global --add safe.directory "$GITHUB_WORKSPACE"
 
 sh -c "/bin/drone-git-push $*"
